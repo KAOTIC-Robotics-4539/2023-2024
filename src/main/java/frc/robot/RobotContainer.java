2bdd34fd2b1e649ed23807df7fb4ad5f4c2e879d
SamzyDev <144393153+sstock2005@users.noreply.github.com>
2023-09-21 22:59:56 -0500

    hotfix1
    - Added PathplannerLib dependancy
    - Added an untested PathPlanner auto
        - You need to generate a path and put it in the ./deploy folder.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,8 +1,19 @@
 package frc.robot;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.auto.PIDConstants;
+import com.pathplanner.lib.auto.SwerveAutoBuilder;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 import frc.robot.autos.*;
@@ -78,6 +89,29 @@ public class RobotContainer {
         //armPickupButton2.onTrue(new lowerArmGoTo(1, 179000, m_lowerArm, false));
     }
 
+
+    private Command getPathPlannerCommand()
+    {
+        List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup("FullAuto", new PathConstraints(4, 3));
+        HashMap<String, Command> eventMap = new HashMap<>();
+        // eventMap.put("marker1", new PrintCommand("Passed marker 1"));
+
+// Create the AutoBuilder. This only needs to be created once when robot code starts, not every time you want to create an auto command. A good place to put this is in RobotContainer along with your subsystems.
+        SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
+            s_Swerve::getPose, // Pose2d supplier
+            s_Swerve::resetOdometry, // Pose2d consumer, used to reset odometry at the beginning of auto
+            Constants.Swerve.swerveKinematics, // SwerveDriveKinematics
+            new PIDConstants(5.0, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
+            new PIDConstants(0.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
+            s_Swerve::setModuleStates, // Module states consumer used to output to the drive subsystem
+            eventMap,
+            true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+            s_Swerve // The drive subsystem. Used to properly set the requirements of path following commands
+        );
+
+        Command fullAuto = autoBuilder.fullAuto(pathGroup);
+        return fullAuto;
+    }
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
      *
@@ -85,6 +119,6 @@ public class RobotContainer {
      */
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
-        return new exampleAuto(s_Swerve);
+        return getPathPlannerCommand();
     }
 }
